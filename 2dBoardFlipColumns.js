009c12b24809af727d627902e94b7f0e61322fa5
Rebecca Gray <24249459+rebecca-gray@users.noreply.github.com>
2018-04-07 05:59:35 -0400

    added explanation for results to 2dBoard

--- 2dBoardFlipColumns.js
+++ 2dBoardFlipColumns.js
@@ -1,5 +1,10 @@
 let _ = require('underscore');
 
+// returns the high score for the board, and a bitwise list of which columns to flip to get that score. 
+// it is worth noting that the inverse of the flip list will return the same score.
+// so a returned value of { count: 2, [1,0,0,1] } indicates that the highest score for the given board is 2 
+// and to get that score, either flip columns 1 & 4 or 2 & 3.
+
 const board = [["x","o",'o',"x"],
                ["o","x","o","o"],
                ["o","x","o","o"]];
